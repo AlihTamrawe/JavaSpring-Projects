4f6abdea041fd0c483d128f224c8b594fe475620
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-11-01 17:02:25 +0200

    Ninja Gold commit

---
+++ NinjaGold/src/main/java/axsos/java/jspdemo/NinjaGoldApplication.java
@@ -0,0 +1,128 @@
+package axsos.java.jspdemo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@SpringBootApplication
+@Controller
+public class NinjaGoldApplication {
+	ArrayList<String> list =new ArrayList<String>();
+	ArrayList<String> list2 =new ArrayList<String>();
+
+	private int gold =0;
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaGoldApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String first() {
+		
+		
+		return "redirect:/gold";
+	}
+	@RequestMapping("/gold")
+	public String show(Model model) {
+		model.addAttribute("list", list);
+		
+		model.addAttribute("gold", this.getGold());
+		return "index.jsp";
+	}
+	
+	@PostMapping("/farm")	
+	public String farm( RedirectAttributes redirectAttributes) {
+		Random rand = new Random();
+		int num = rand.nextInt(10)+10;
+		this.setGold(num+this.getGold());
+		LocalDateTime l = LocalDateTime.now();
+		list.add("You Entered a Farm and earned "+num+"gold.  "+l);
+		
+		
+		redirectAttributes.addFlashAttribute("colur","green");
+
+		
+		
+		return "redirect:/gold";
+	}
+	@PostMapping("/cave")	
+	public String cave( RedirectAttributes redirectAttributes) {
+		Random rand = new Random();
+		int num = rand.nextInt(10)+10;
+		this.setGold(num+this.getGold());
+		LocalDateTime l = LocalDateTime.now();
+		list.add("You Entered a Farm and earned "+num+"gold.  "+l);
+		
+		
+		redirectAttributes.addFlashAttribute("colur","green");
+
+		
+		
+		
+		return "redirect:/gold";
+	}
+	@PostMapping("/house")	
+	public String house(RedirectAttributes redirectAttributes) {
+		
+		Random rand = new Random();
+		int num = rand.nextInt(10)+10;
+		this.setGold(num+this.getGold());
+		LocalDateTime l = LocalDateTime.now();
+		list.add("You Entered a House and earned "+num+" gold.  "+l);
+		
+		
+		redirectAttributes.addFlashAttribute("colur","green");
+
+		
+		
+		return "redirect:/gold";
+	}
+	@PostMapping("/quest")	
+	public String quest( RedirectAttributes redirectAttributes) {
+		
+		String style="green";
+		Random rand = new Random();
+		int ran = rand.nextInt(1);
+		int num =0;
+		if(ran==0) {
+			num=0;
+					
+		}
+		else {
+			num=50;
+		}
+		this.setGold(num+this.getGold());
+		LocalDateTime l = LocalDateTime.now();
+		
+		
+		if(num==0) {
+			list2.add("You lost a Quest and earned "+num+" gold.  "+l);			
+
+		}
+		else {
+			list.add("You Entered a Quest and earned "+num+" gold.  "+l);
+
+		}
+		
+		redirectAttributes.addFlashAttribute("list2",list2);
+
+		
+		return "redirect:/gold";
+	}
+	public int getGold() {
+		return gold;
+	}
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+}
---
+++ NinjaGold/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,87 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+ <link rel="stylesheet" type="text/css" href="/CSS/style.css">
+        <link rel="stylesheet" href="/webjars/bootstrap/css/bootstrap.min.css" />
+<title>Gold Ninja </title>
+
+</head>
+<body>
+
+<div class="d-flex container ">
+		<h3>Golds:</h3>
+		<input placeholder="0" value="${gold}" readonly /> 
+			</div>
+	
+	<div class="d-flex container-xxl .justify-content-sm-between col">
+	
+	<div class="card d-flex align-items-center d-flex "  style="width: 20rem; height:20rem" >
+  	<div class="card-body">
+    	<h5 class="card-title d-flex justify-content-center">Farm</h5>
+    	<h5 class="card-text"> (Earns 10-20 gold)</h5>
+		
+		<form action="/farm" method="post">
+    	<button class="btn btn-primary ">Find Gold</button>
+    	</form>
+		
+	  	</div>
+	</div>
+		<div class="card d-flex align-items-center" style="width: 20rem; height:20rem" >
+  	<div class="card-body">
+    	<h5 class="card-title d-flex justify-content-center">Cave</h5>
+    	<h5 class="card-text"> (Earns 10-20 gold)</h5>
+		<form action="/cave" method="post">
+    	<button class="btn btn-primary ">Find Gold</button>
+    	</form>
+  	</div>
+	</div>
+		<div class="card d-flex align-items-center" style="width: 20rem; height:20rem" >
+  	<div class="card-body ">
+    	<h5 class="card-title d-flex justify-content-center">House</h5>
+    	<h5 class="card-text"> (Earns 10-20 gold)</h5>
+		<form action="/house" method="post">
+    	<button class="btn btn-primary ">Find Gold</button>
+    	</form>
+  	</div>
+	</div>
+		<div class="card d-flex " style="width: 20rem; height:20rem" >
+  	<div class="card-body">
+    	<h5 class="card-title d-flex justify-content-center">Quest</h5>
+    	<h5 class="card-text"> (Earns 0-50 gold)</h5>
+    	<form action="/quest" method="post">
+    	<button class="btn btn-primary ">Find Gold</button>
+    	</form>
+  	</div>
+	</div>
+		
+	</div>
+	
+	
+	<div class="d-flex container row " >
+		<h3>Activities:</h3>
+		<div  >
+		
+		<c:forEach var="item" items="${list}">
+		<p style="color:green">
+		<c:out value="${item}" />
+		</p>
+	
+		</c:forEach>
+		<c:forEach var="item" items="${list2}">
+		<p style="color:red">
+		<c:out value="${item}" />
+		</p>
+	
+		</c:forEach>
+		</div>
+			</div>
+	
+
+</body>
+</html>
