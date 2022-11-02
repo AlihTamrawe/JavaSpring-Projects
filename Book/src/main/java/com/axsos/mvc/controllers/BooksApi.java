bd0746537a13bc99589230821a2c0f9ba2283068
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-11-02 18:54:43 +0200

    Book Model commit

---
+++ Book/src/main/java/com/axsos/mvc/controllers/BooksApi.java
@@ -0,0 +1,57 @@
+package com.axsos.mvc.controllers;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.axsos.mvc.models.Book;
+import com.axsos.mvc.services.BookService;
+@RestController
+public class BooksApi {
+    private final BookService bookService;
+    public BooksApi(BookService bookService){
+        this.bookService = bookService;
+    }
+    @RequestMapping("")
+    public void welcome(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/api/books");
+    }
+    @RequestMapping("/api/books")
+    public List<Book> index() {
+        return bookService.allBooks();
+    }
+    
+    @RequestMapping(value="/api/books", method=RequestMethod.POST)
+    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = new Book(title, desc, lang, numOfPages);
+        return bookService.createBook(book);
+    }
+    
+    @RequestMapping("/api/books/{id}")
+    public Book show(@PathVariable("id") Long id) {
+        Book book = bookService.findBook(id);
+        return book;
+    }
+    
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
+    public void destroy(@PathVariable("id") Long id ,HttpServletResponse response) throws IOException {
+       
+    
+        bookService.deleteBook(id);
+        
+        response.sendRedirect("/showdelete");
+        
+    }
+    
+    @RequestMapping("/showdelete")
+    public String show() {
+     
+        return "Delete it successufully";
+    }
+}
